3fc8f1597cf0b1e5508a6244041df4ea1c0eca6e
iSylvester666 <damyanvincent7@gmail.com>
2025-10-17 22:19:56 +0700

    new 2

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 – Load Orion Library
-local OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/jensonhirst/Orion/main/source'))()
+local OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Orion/main/source'))()
 
 – Create Window
 local Window = OrionLib:MakeWindow({
